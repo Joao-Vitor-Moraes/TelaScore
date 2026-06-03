deb04016b92d7e2ab9c8b5703d3a6103d6528835
Joao-Vitor-Moraes <jvmb@cesar.school>
2026-06-03 18:20:05 -0300

    registros do config

--- TelaScore/infraestrutura/src/main/java/com/requisitos/avaliacaofilmes/TelaScore/infraestrutura/config/InfraestruturaConfig.java
+++ TelaScore/infraestrutura/src/main/java/com/requisitos/avaliacaofilmes/TelaScore/infraestrutura/config/InfraestruturaConfig.java
@@ -1,14 +1,31 @@
 package com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.config;
 
 import com.requisitos.avaliacaofilmes.TelaScore.aplicacao.identidade.GeradorId;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.analise.meta.MetaRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.analise.quiz.QuizRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.analise.recomendacao.RecomendacaoRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.avaliacao.AvaliacaoRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.diretor.DiretorRepositorio;
 import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.filme.FilmeRepositorio;
 import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.lista.ListaRepositorio;
 import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.lista.ListaServico;
 import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.solicitacao.SolicitacaoRepositorio;
 import com.requisitos.avaliacaofilmes.TelaScore.dominio.catalogo.solicitacao.SolicitacaoServico;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.informacao.noticia.NoticiaRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.social.comunidade.ComunidadeRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.dominio.social.mensagem.MensagemRepositorio;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.analise.meta.MetaRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.analise.quiz.QuizRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.analise.recomendacao.RecomendacaoRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.catalogo.avaliacao.AvaliacaoRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.catalogo.diretor.DiretorRepositorioImpl;
 import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.catalogo.filme.FilmeRepositorioImpl;
 import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.catalogo.lista.ListaRepositorioImpl;
 import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.catalogo.solicitacao.SolicitacaoRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.informacao.noticia.NoticiaRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.social.comunidade.ComunidadeRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.social.mensagem.MensagemRepositorioImpl;
+import com.requisitos.avaliacaofilmes.TelaScore.infraestrutura.social.mensagem.MensagemRepositorioLoggingDecorator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +38,7 @@ public class InfraestruturaConfig {
         return new GeradorIdImpl();
     }
 
+    // catalogo
     @Bean
     public ListaRepositorio listaRepositorio() {
         return new ListaRepositorioImpl();
@@ -36,6 +54,51 @@ public class InfraestruturaConfig {
         return new FilmeRepositorioImpl();
     }
 
+    @Bean
+    public AvaliacaoRepositorio avaliacaoRepositorio() {
+        return new AvaliacaoRepositorioImpl();
+    }
+
+    @Bean
+    public DiretorRepositorio diretorRepositorio() {
+        return new DiretorRepositorioImpl();
+    }
+
+    // analise
+    @Bean
+    public MetaRepositorio metaRepositorio() {
+        return new MetaRepositorioImpl();
+    }
+
+    @Bean
+    public RecomendacaoRepositorio recomendacaoRepositorio() {
+        return new RecomendacaoRepositorioImpl();
+    }
+
+    @Bean
+    public QuizRepositorio quizRepositorio() {
+        return new QuizRepositorioImpl(ConexaoBanco.obterEntityManager());
+    }
+
+    // informacao
+    @Bean
+    public NoticiaRepositorio noticiaRepositorio() {
+        return new NoticiaRepositorioImpl();
+    }
+
+    // social
+    @Bean
+    public ComunidadeRepositorio comunidadeRepositorio() {
+        return new ComunidadeRepositorioImpl();
+    }
+
+    @Bean
+    public MensagemRepositorio mensagemRepositorio() {
+        return new MensagemRepositorioLoggingDecorator(
+                new MensagemRepositorioImpl(ConexaoBanco.obterEntityManager()));
+    }
+
+    // servicos de dominio
     @Bean
     public ListaServico listaServico(ListaRepositorio listaRepositorio) {
         return new ListaServico(listaRepositorio);
