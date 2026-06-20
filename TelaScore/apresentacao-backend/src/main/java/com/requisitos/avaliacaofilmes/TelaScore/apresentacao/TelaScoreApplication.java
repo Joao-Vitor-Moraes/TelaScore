a7661896cfc28c42c07b0d76883fe15134d395d7
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2026-06-20 01:09:33 -0300

    feat: tratar devtools para finalização de projeto

--- TelaScore/apresentacao-backend/src/main/java/com/requisitos/avaliacaofilmes/TelaScore/apresentacao/TelaScoreApplication.java
+++ TelaScore/apresentacao-backend/src/main/java/com/requisitos/avaliacaofilmes/TelaScore/apresentacao/TelaScoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 })
 public class TelaScoreApplication {
     public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
+
         org.springframework.boot.SpringApplication.run(TelaScoreApplication.class, args);
     }
 }
